3e65b9275513a636499f6b6434411bba650294b2
[dyad] <cae.arcoverde@gmail.com>
2025-12-02 11:15:34 -0300

    [dyad] Corrigindo a falha de estilo ao adicionar a importação do index.css no ponto de entrada do aplicativo. - wrote 1 file(s)

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // Adicionando a importação do CSS principal
 import './print.css';
 
 const rootElement = document.getElementById('root');
